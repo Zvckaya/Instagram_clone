fb6cfce810cbdd0839c9ba3931b1d53df97e7b8a
lzvckaya <lzvckaya@gmail.com>
2023-07-13 17:24:18 +0900

    set firebase setting

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
 import 'package:instagram_clone/responsive/web_screen_layout.dart';
 import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/utils/keys.dart';
 
-void main() {
+void main() async {
+  if (kIsWeb) {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: appApiKey,
+            authDomain: appAuthDomain,
+            projectId: appProjectId,
+            storageBucket: appStorageBucket,
+            messagingSenderId: appMessagingSenderId,
+            appId: appKey));
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
---
+++ lib/utils/keys.dart
@@ -0,0 +1,6 @@
+const appApiKey = "AIzaSyBPFLGtWsTWNrV6nT2JpKX_T125aM3lTjc";
+const appAuthDomain = "instagram-tut-fe001.firebaseapp.com";
+const appProjectId = "instagram-tut-fe001";
+const appStorageBucket = "instagram-tut-fe001.appspot.com";
+const appMessagingSenderId = "49197269334";
+const appKey = "1:49197269334:web:dee126086118d17214d6f7";
