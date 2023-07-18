890b3388f6cf5b073e0999f7acf343c9f77af14b
lzvckaya <lzvckaya@gmail.com>
2023-07-18 18:26:07 +0900

    add post system

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,7 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/provider/user_provider.dart';
+import 'package:instagram_clone/screens/add_post_screen.dart';
+import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/utils/dimesions.dart';
 import 'package:provider/provider.dart';
 import 'package:instagram_clone/models/user.dart' as model;
 
@@ -13,6 +17,31 @@ class MobileScreenLayout extends StatefulWidget {
 }
 
 class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+  late PageController pageController;
+  int _page = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  void navigationTap(int page) {
+    pageController.jumpToPage(page);
+  }
+
+  void onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final model.User? user = Provider.of<UserProvider>(context).getUser;
@@ -21,7 +50,57 @@ class _MobileScreenLayoutState extends State<MobileScreenLayout> {
             child: CircularProgressIndicator(),
           )
         : Scaffold(
-            body: Text(user.email),
-          );
+            body: PageView(
+              physics: NeverScrollableScrollPhysics(),
+              children: homeScreenItems,
+              controller: pageController,
+              onPageChanged: onPageChanged,
+            ),
+            bottomNavigationBar: CupertinoTabBar(
+              backgroundColor: mobileBackgroundColor,
+              items: [
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.home,
+                    color: _page == 0 ? primaryColor : secondaryColor,
+                  ),
+                  label: '',
+                  backgroundColor: primaryColor,
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.search,
+                    color: _page == 1 ? primaryColor : secondaryColor,
+                  ),
+                  label: '',
+                  backgroundColor: primaryColor,
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.add_circle,
+                    color: _page == 2 ? primaryColor : secondaryColor,
+                  ),
+                  label: '',
+                  backgroundColor: primaryColor,
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.favorite,
+                    color: _page == 3 ? primaryColor : secondaryColor,
+                  ),
+                  label: '',
+                  backgroundColor: primaryColor,
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.person,
+                    color: _page == 4 ? primaryColor : secondaryColor,
+                  ),
+                  label: '',
+                  backgroundColor: primaryColor,
+                )
+              ],
+              onTap: navigationTap,
+            ));
   }
 }
--- lib/utils/dimesions.dart
+++ lib/utils/dimesions.dart
@@ -1,3 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/screens/add_post_screen.dart';
 
 const webScreenSize = 600;
+
+const homeScreenItems = [
+  Center(child: Text('home')),
+  Center(child: Text("search")),
+  AddPostScreen(),
+  Center(child: Text("like")),
+  Center(child: Text("user")),
+];
