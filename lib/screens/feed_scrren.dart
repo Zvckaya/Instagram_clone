1f1eaa232e3a85e94078ee17d9e2bfa4dd549e7a
lzvckaya <lzvckaya@gmail.com>
2023-07-19 18:04:33 +0900

    add post_card

---
+++ lib/screens/feed_scrren.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/widgets/post_cart.dart';
+
+class FeedScreen extends StatelessWidget {
+  const FeedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        centerTitle: false,
+        title: SvgPicture.asset(
+          'assets/ic_instagram.svg',
+          color: primaryColor,
+          height: 32,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.message_outlined),
+          )
+        ],
+      ),
+      body: PostCart(),
+    );
+  }
+}
--- lib/utils/dimesions.dart
+++ lib/utils/dimesions.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/screens/add_post_screen.dart';
+import 'package:instagram_clone/screens/feed_scrren.dart';
 
 const webScreenSize = 600;
 
 const homeScreenItems = [
-  Center(child: Text('home')),
+  FeedScreen(),
   Center(child: Text("search")),
   AddPostScreen(),
   Center(child: Text("like")),
